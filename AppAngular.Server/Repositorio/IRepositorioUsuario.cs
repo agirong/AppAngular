2ad504db68116284e54c13124795a677c8bd9ed5
agirong <aogarrido.g@gmail.com>
2025-02-08 18:12:36 -0600

    Crear Repositorio con interface.

---
+++ AppAngular.Server/Repositorio/IRepositorioUsuario.cs
@@ -0,0 +1,13 @@
+﻿using AppAngular.Server.Models;
+
+namespace AppAngular.Server.Repositorio
+{
+    public interface IRepositorioUsuario
+    {
+        List<Usuario> GetUsuarios();    
+        void CreateUsuario(Usuario usuario);
+        void UpdateUsuario(int id, Usuario usuarioActualizado);
+        void DeleteUsuario(int id);
+        Usuario GetUsuarioById(int id);
+    }
+}
---
+++ AppAngular.Server/RepositorioImp/RepositorioUsuarioImp.cs
@@ -0,0 +1,99 @@
+﻿using AppAngular.Server.Models;
+using AppAngular.Server.Repositorio;
+
+namespace AppAngular.Server.RepositorioImp
+{
+    public class RepositorioUsuarioImp : IRepositorioUsuario
+    {
+        private readonly ModelContext _context;
+
+        public RepositorioUsuarioImp(ModelContext context)
+        {
+            _context = context; 
+        }
+
+        public List<Usuario> GetUsuarios() 
+        {
+            try
+            {
+                return _context.Usuarios.ToList();  
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception(ex.Message);                
+            }
+        }
+
+        public void CreateUsuario(Usuario usuario)
+        {
+            try
+            {
+                _context.Usuarios.Add(usuario);
+                _context.SaveChanges(); 
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void UpdateUsuario(int id, Usuario usuarioActualizado)
+        {
+            try
+            {
+                var usuarioExistente = _context.Usuarios.Find(id);
+                if (usuarioExistente == null)
+                {
+                    throw new Exception("Usuario no encontrado");
+                }
+                // Actualiza las propiedades del usuario existente
+                usuarioExistente.Nombre = usuarioActualizado.Nombre;
+                usuarioExistente.Apellido = usuarioActualizado.Apellido;
+                usuarioExistente.Email = usuarioActualizado.Email;
+                usuarioExistente.Contrasena = usuarioActualizado.Contrasena;
+                usuarioExistente.Estado = usuarioActualizado.Estado;
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void DeleteUsuario(int id)
+        {
+            try
+            {
+                var usuarioExistente = _context.Usuarios.Find(id);
+                if (usuarioExistente == null)
+                {
+                    throw new Exception("Usuario no encontrado");
+                }
+                _context.Usuarios.Remove(usuarioExistente);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Usuario GetUsuarioById(int id)
+        {
+            try
+            {
+                var usuario = _context.Usuarios.Find(id);
+                if (usuario == null)
+                {
+                    throw new Exception("Usuario no encontrado");
+                }
+                return usuario;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al obtener el usuario: " + ex.Message);
+            }
+        }
+
+    }
+}
