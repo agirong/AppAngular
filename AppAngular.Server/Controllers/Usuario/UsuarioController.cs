41636bee015aa9108e31cce448bed994afa96590
agirong <aogarrido.g@gmail.com>
2025-02-08 22:20:33 -0600

    Crear controlador de usuario

---
+++ AppAngular.Server/Controllers/Usuario/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using AppAngular.Server.Servicio;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AppAngular.Server.Controllers.Usuario
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IServicioUsuario _servicioUsuario;
+
+        public UsuarioController(IServicioUsuario servicioUsuario) 
+        {
+            _servicioUsuario = servicioUsuario; 
+        }  
+
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public IEnumerable<Models.Usuario> Get()
+        {
+            return _servicioUsuario.GetUsers();
+        }
+
+        // GET api/<UsuarioController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsuarioController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsuarioController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsuarioController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
